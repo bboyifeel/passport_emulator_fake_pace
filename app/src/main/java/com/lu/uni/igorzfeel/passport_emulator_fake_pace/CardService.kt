ecf5a49432861df65a0f2761c9709ae32f8bd4fd
Igor <bboyifeel@gmail.com>
2020-06-18 12:55:49 +0200

    + pace infos

--- app/src/main/java/com/lu/uni/igorzfeel/passport_emulator_fake_pace/CardService.kt
+++ app/src/main/java/com/lu/uni/igorzfeel/passport_emulator_fake_pace/CardService.kt
@@ -10,10 +10,15 @@ class CardService: HostApduService() {
     companion object {
         val TAG: String  = "CardService"
 
-        val OK_CAPDU        = Utils.hexStringToByteArray("9000")
-        val FAILED_CAPDU    = Utils.hexStringToByteArray("6F00")
-        val UNKNOWN_CAPDU   = Utils.hexStringToByteArray("0000")
+        val OK_RAPDU        = Utils.hexStringToByteArray("9000")
+        val FAILED_RAPDU    = Utils.hexStringToByteArray("6F00")
+        val UNKNOWN_RAPDU   = Utils.hexStringToByteArray("0000")
         val SELECT_CAPDU    = Utils.hexStringToByteArray("00A4040C07A0000002471001")
+        val CA1_CAPDU       = Utils.hexStringToByteArray("00A4020C02011C")
+        val CA2_CAPDU       = Utils.hexStringToByteArray("00B0000008")
+        val CA2_RAPDU       = Utils.hexStringToByteArray("31143012060A04009000")
+        val CA3_CAPDU       = Utils.hexStringToByteArray("00B000080E")
+        val CA3_RAPDU       = Utils.hexStringToByteArray("7F000702020402040201020201109000")
     }
 
 
@@ -23,17 +28,26 @@ class CardService: HostApduService() {
     override fun processCommandApdu(commandApdu: ByteArray?, extras: Bundle?): ByteArray {
 
         if (commandApdu == null)
-            return UNKNOWN_CAPDU
+            return UNKNOWN_RAPDU
 
-        var response = FAILED_CAPDU
+        var response = FAILED_RAPDU
         updateLog("Received APDU: " + Utils.toHex(commandApdu))
 
         if (Arrays.equals(SELECT_CAPDU, commandApdu)) {
             updateLog( "This is a SELECT_APDU")
-            response = OK_CAPDU
+            response = OK_RAPDU
+        }
+        else if (Arrays.equals(CA1_CAPDU, commandApdu)) {
+            response = OK_RAPDU
+        }
+        else if (Arrays.equals(CA2_CAPDU, commandApdu)) {
+            response = CA2_RAPDU
+        }
+        else if (Arrays.equals(CA3_CAPDU, commandApdu)) {
+            response = CA3_RAPDU
         }
         else {
-            response = UNKNOWN_CAPDU
+            response = UNKNOWN_RAPDU
         }
 
         updateLog("Sending: " + Utils.toHex(response))
@@ -42,8 +56,17 @@ class CardService: HostApduService() {
 
 
     fun updateLog(msg: String) {
-        System.out.println(msg)
+//        System.out.println(msg)
         Log.i(TAG, msg)
     }
 
 }
+
+
+//// msg flow
+//[ReadBinaryAPDUSender][sendSelectFile] Sending: 00A4020C02011C
+//[ReadBinaryAPDUSender][sendSelectFile] Received: 9000
+//[ReadBinaryAPDUSender][sendReadBinary] Sending: 00B0000008
+//[ReadBinaryAPDUSender][sendReadBinary] Received: 31143012060A04009000
+//[ReadBinaryAPDUSender][sendReadBinary] Sending: 00B000080E
+//[ReadBinaryAPDUSender][sendReadBinary] Received: 7F000702020402040201020201109000
--- app/src/main/java/com/lu/uni/igorzfeel/passport_emulator_fake_pace/MainActivity.kt
+++ app/src/main/java/com/lu/uni/igorzfeel/passport_emulator_fake_pace/MainActivity.kt
@@ -3,6 +3,7 @@ package com.lu.uni.igorzfeel.passport_emulator_fake_pace
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
